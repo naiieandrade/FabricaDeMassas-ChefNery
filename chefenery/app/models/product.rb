8cb2e531268e50086edd6121e91c2c4ab4e30c4e
xxxxx <xxxx.@gmail.com>
2017-03-27 10:59:54 -0300

    Initial commit

---
+++ chefenery/app/models/product.rb
@@ -0,0 +1,10 @@
+class Product < ActiveRecord::Base
+  has_and_belongs_to_many :ingredients
+  enum category: ["Culinária Italiana", "Culinária Oriental", "Culinária Árabe", "Culinária Brasileira"]
+
+  TITLE_MIN_LENGTH = 3
+  DESCRIPTION_MIN_LENGTH = 2
+
+  validates :title, presence: true, :length => {:minimum => TITLE_MIN_LENGTH}, uniqueness: true
+  validates :description, presence: true, :length => {:minimum => DESCRIPTION_MIN_LENGTH}
+end
