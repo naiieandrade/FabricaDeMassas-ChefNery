a3ffd7ea8fd8d948f90fb90c2de8bd20e864c189
Harrison <hpedro1195@gmail.com>
2017-04-08 09:49:50 -0300

    Create sessions for log_in users and log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   resources :ingredients
@@ -6,6 +8,11 @@ Rails.application.routes.draw do
   resources :users
 
   root "home#index"
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   get 'home/about'
   get 'home/contact'
