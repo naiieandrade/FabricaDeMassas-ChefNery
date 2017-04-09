ff7270d6648f37c45b7116674a9beeac2c5fa188
Felipe <felipe.osorio72@outlook.com.br>
2017-04-08 22:11:11 -0300

    Changing .html to .erb and refactoring those

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def getImages
+		images = []
+
+		images << '/assets/image0.jpg'
+		images << '/assets/image1.jpg'
+		images << '/assets/image2.jpg'
+		images << '/assets/image3.jpg'
+		images << '/assets/image4.jpg'
+	end
 end
