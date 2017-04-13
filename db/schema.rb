758f49defad62430e48196a39d813d9f424d66a5
Felipe <felipe.osorio72@outlook.com.br>
2017-04-13 12:40:28 -0300

    Adding column and a methods in helper to verify admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410030910) do
+ActiveRecord::Schema.define(version: 20170413145705) do
 
   create_table "Orders_Products", id: false, force: :cascade do |t|
     t.integer "order_id",   null: false
@@ -64,8 +64,9 @@ ActiveRecord::Schema.define(version: 20170410030910) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "is_administrator", default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
