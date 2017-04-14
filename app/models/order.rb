c0c778068db790a1a8577874be5415e86e356e2a
Mateus Andrade <mateusandrade080@gmail.com>
2017-04-14 18:20:12 -0300

    Add OrderItem model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,15 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
-	has_and_belongs_to_many :products
+	has_many :order_items
 
 	validates :shippment_address, presence: true
+
+	def subtotal
+		orders_items.collect { |order_item| order_item.valid? ? (order_item.quantity * order_item.unit_price} : 0}.sum
+	end
+
+	def
+		self[:subtotal] = subtotal
+	end
+
 end
