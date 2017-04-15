0e1e2211f48842ba6215e0e584090bc0c6e9aabf
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-04-15 12:13:48 -0300

    Fixing an error on seT_product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -51,7 +51,7 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find_by(:id)
+      @product = Product.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
