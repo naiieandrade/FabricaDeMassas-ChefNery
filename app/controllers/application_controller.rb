79185cff82f21276b9b0e409108910d9bfeb4cc3
Felipe <felipe.osorio72@outlook.com.br>
2017-04-20 21:24:07 -0300

    Created a couriers crud and limited its access for normal users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def home_if_not_admin
+    if current_user.nil? || !is_administrator(current_user)
+      redirect_to root_url
+    end
+  end
+
   include SessionsHelper
   include OrdersHelper
 
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170422220322) do
   add_index "Orders_Products", ["order_id", "product_id"], name: "index_Orders_Products_on_order_id_and_product_id"
   add_index "Orders_Products", ["product_id", "order_id"], name: "index_Orders_Products_on_product_id_and_order_id"
 
+  create_table "couriers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ingredients", force: :cascade do |t|
     t.string   "title"
     t.float    "price"
