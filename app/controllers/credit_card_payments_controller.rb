9f1cfb86c53c5fbb5f79b20a3d2d5f39c79cebe9
Mateus Andrade <mateusandrade080@gmail.com>
2017-05-10 22:40:52 -0300

    Change Payment to CreditCardPayment

---
+++ app/controllers/credit_card_payments_controller.rb
@@ -0,0 +1,74 @@
+class CreditCardPaymentsController < ApplicationController
+  before_action :set_credit_card_payment, only: [:show, :edit, :update, :destroy]
+
+  # GET /credit_card_payments
+  # GET /credit_card_payments.json
+  def index
+    @credit_card_payments = CreditCardPayment.all
+  end
+
+  # GET /credit_card_payments/1
+  # GET /credit_card_payments/1.json
+  def show
+  end
+
+  # GET /credit_card_payments/new
+  def new
+    @credit_card_payment = CreditCardPayment.new
+  end
+
+  # GET /credit_card_payments/1/edit
+  def edit
+  end
+
+  # POST /credit_card_payments
+  # POST /credit_card_payments.json
+  def create
+    @credit_card_payment = CreditCardPayment.new(credit_card_payment_params)
+
+    respond_to do |format|
+      if @credit_card_payment.save
+        format.html { redirect_to @credit_card_payment, notice: 'Credit Card Payment was successfully created.' }
+        format.json { render :show, status: :created, location: @credit_card_payment }
+      else
+        format.html { render :new }
+        format.json { render json: @credit_card_payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /credit_card_payments/1
+  # PATCH/PUT /credit_card_payments/1.json
+  def update
+    respond_to do |format|
+      if @credit_card_payment.update(credit_card_payment_params)
+        format.html { redirect_to @credit_card_payment, notice: 'Credit Card Payment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @credit_card_payment }
+      else
+        format.html { render :edit }
+        format.json { render json: @credit_card_payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /credit_card_payments/1
+  # DELETE /credit_card_payments/1.json
+  def destroy
+    @credit_card_payment.destroy
+    respond_to do |format|
+      format.html { redirect_to credit_card_payments_url, notice: 'Credit Card Payment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_credit_card_payment
+      @credit_card_payment = CreditCardPayment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def credit_card_payment_params
+      params.require(:credit_card_payment).permit(:amount, :responseCode, :responseMessage, :xref, :amountReceived, :transactionID, :cardNumberMask, :cardTypeCode, :cardType)
+    end
+end
---
+++ app/views/credit_card_payments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "credit_card_payments/payment", credit_card_payment: @credit_card_payment
