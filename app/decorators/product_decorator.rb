f8970dbf1c37558fb95ef220eece7eefe03ff95b
danmoura17 <danmoura17@gmail.com>
2017-05-11 18:47:12 -0300

    Adding new decorator background according with category

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -4,6 +4,14 @@ module ProductDecorator
     end
 
     def category_decorator(category)
-        image_tag("/assets/#{category}.png", :size => "40x40")
+        image_tag("/assets/iconCategory/#{category}.png", :size => "40x40")
     end
+
+    def set_background(category)
+        category_css = category.mb_chars.normalize(:kd).gsub(/[^x00-\x7F]/n, '').to_s.downcase
+        link_to image_tag("/assets/backgroundCategory/", class: category_css),{controller: "products" , action: "show_category"}
+    end
+
+
+
 end
